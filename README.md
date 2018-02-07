b0754074ff8fadecbb720a630c134a3a2b9e9586
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2018-02-07 14:41:44 +0100

    :tada: initial CLI.

---
+++ README.md
@@ -0,0 +1,8 @@
+# HTTTP Codes
+
+A super simple CLI to look up the meaning of status codes.
+
+```
+npm i -g http-code
+http-code 200
+```
---
+++ index.js
@@ -0,0 +1,32 @@
+#! /usr/bin/env node
+
+const cli = require('commander')
+let statusCode = null
+
+cli
+  .version(require('./package.json').version)
+  .arguments('<status code>')
+  .action(code => {
+    statusCode = code
+  })
+  .parse(process.argv)
+
+if (!statusCode) {
+  console.log('No status Code supplied. Please specify a status code you are looking for.')
+  process.exit(1)
+}
+
+const codes = require('./codes.json')
+statusCode = parseInt(statusCode)
+
+const code = codes.find(code => code.number === statusCode)
+if (!code) {
+  console.log('Could not find the code you are looking for.\nIf you think it should exist please consider opening an Issue.')
+  process.ecit(1)
+}
+
+console.log(`
+${code.number}: ${code.name}
+
+${code.description}
+`)
